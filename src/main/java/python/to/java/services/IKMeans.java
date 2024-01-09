cf4bd32acc1be927bc933f21a2e4d9bdd4f8c0ea
AllanCapistrano <allan.capistrano3@gmail.com>
2024-01-09 10:39:17 -0300

    docs: execute method

--- src/main/java/python/to/java/services/IKMeans.java
+++ src/main/java/python/to/java/services/IKMeans.java
@@ -3,5 +3,13 @@ package python.to.java.services;
 import java.util.List;
 
 public interface IKMeans {
+  /**
+   * Executa o algoritmo KMeans em uma lista contendo a credibilidade dos nós,
+   * e retorna uma lista contendo somente as credibilidades que compõem o grupo
+   * com as maiores credibilidades.
+   *
+   * @param nodesCredibilityString String - Lista com as credibilidades dos nós.
+   * @return List<Float>
+   */
   public List<Float> execute(String nodesCredibilityString);
 }
